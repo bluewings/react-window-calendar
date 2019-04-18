216f0f9a3a63a5ddef1f524cc999df0377916183
sungwon.cha <mygenie3@naver.com>
2019-04-18 15:06:29 +0900

    fix

--- src/core/components/WindowCalendar/WindowCalendar.tsx
+++ src/core/components/WindowCalendar/WindowCalendar.tsx
@@ -2,8 +2,40 @@ import * as React from 'react';
 import { WindowGrid } from 'react-window-grid';
 import { FunctionComponent, useMemo, SyntheticEvent, useState, useRef } from 'react';
 import { useColumns, useEventHandlers, useRows } from '../../hooks';
-
+import { format, addMonths, differenceInMonths } from 'date-fns';
 type ScrollEvent = SyntheticEvent<HTMLDivElement>;
+
+type WindowCalendarProps222 = {
+  scrollTop?: number;
+  scrollLeft?: number;
+  width?: number;
+  height?: number;
+
+  columns?: any;
+  columnCount: number;
+  columnWidth: number | Function;
+
+  rows?: any;
+  rowCount: number;
+  rowHeight: number | Function;
+
+  fixedTopCount?: number;
+  fixedLeftCount?: number;
+  fixedRightCount?: number;
+  fixedBottomCount?: number;
+  overscanCount?: number;
+
+  fillerColumn?: 'none' | 'append' | 'stretch' | 'shrink';
+  fillerRow?: 'none' | 'append' | 'stretch' | 'shrink';
+  /** 스크롤되는 뷰포트 너비가 특정값 이하로 떨어지면 fixedColumn 이 무효화된다. */
+  minVisibleScrollViewWidth: number;
+  minVisibleScrollViewHeight: number;
+
+  containerStyle?: any;
+  guideline?: boolean;
+
+};
+
 
 type WindowCalendarProps = {
   scrollTop?: number;
@@ -34,168 +66,69 @@ type WindowCalendarProps = {
   containerStyle?: any;
   guideline?: boolean;
 
-  // maxScrollY?: number
-  // maxScrollX?: number
+  minDate?: string;
+  maxDate?: string;
 
-  // cellStyle?: string;
-
-  // columns: array
-
-  // containerStyle?: string;
-  // guidelineStyle?: Function;
 };
 
-// function useHandle(callback) {
-//   const handle = useRef();
-//   handle.current = callback;
-//   return (...args) => {
-//     if (typeof handle.current === 'function') {
-//       handle.current(...args);
-//     }
-//   };
-// }
-
 const WindowCalendar: FunctionComponent<WindowCalendarProps> = (props) => {
-  const [columns, columnWidth] = useColumns(props);
-  const rows = useRows(props, columns);
-  // return <pre>{JSON.stringify(rows, null, 2)}</pre>;
-  // const render
-  // const render = (data, columnIndex) => {
-  //   // columns[columnIndex].render
-  // }
-  const [hover, setHover] = useState({ rowIndex: null, columnIndex: null });
-  const currHover = useRef(hover);
-  currHover.current = hover;
-  const handleRef = useRef(setHover);
-  handleRef.current = setHover;
+  let { minDate, maxDate } = props;
 
-  const timer = useRef();
+  minDate = minDate || new Date(2018, 0, 15)
+  maxDate = maxDate || new Date(2020, 11, 15)
 
-  const styleRef = useRef();
-
-  const ownEvents = useMemo(() => {
-    return {
-      mouseover: {
-        '.cell[data-row-index][data-column-index]': (event, ui) => {
-          timer.current && clearTimeout(timer.current);
-          // console.log('%cmouseover', 'background:orange');
-          styleRef.current.innerHTML = '';
-
-          var styleNode = document.createElement('style');
-          styleNode.type = 'text/css';
-          var styleText = document.createTextNode(
-            `[data-row-index="${ui.rowIndex}"] { background:#e9eaeb !important } `,
-          );
-          styleNode.appendChild(styleText);
-          styleRef.current.appendChild(styleNode);
-          // // browser detection (based on prototype.js)
-          // if(!!(window.attachEvent && !window.opera)) {
-          //      styleNode.styleSheet.cssText = 'span { color: rgb(255, 0, 0); }';
-          // } else {
-          //      var styleText = document.createTextNode('span { color: rgb(255, 0, 0); } ');
-          //      styleNode.appendChild(styleText);
-          // }
-          //     var css = 'h1 { background: red; }',
-          //     head = document.head || document.getElementsByTagName('head')[0],
-          //     style = document.createElement('style');
-
-          // head.appendChild(style);
-
-          // style.type = 'text/css';
-          // if (style.styleSheet){
-          //   // This is required for IE8 and below.
-          //   style.styleSheet.cssText = css;
-          // } else {
-          //   style.appendChild(document.createTextNode(css));
-          // }
-
-          // style.appendChild(document.createTextNode(css));
-          // window._cnt1 = (window._cnt1 || 0);
-          // console.log('hover', window._cnt1++)
-          // if (currHover.current.ho)
-          // if (currHover.curren)
-          // styleRef.current.inner
-          // const _ = currHover.current
-          // const { rowIndex, columnIndex } = ui;
-          // if (_.rowIndex !== rowIndex || _.columnIndex !== columnIndex) {
-          //   handleRef.current({ rowIndex, columnIndex });
-          // }
-          // console.log(ui);
-        },
-      },
-      mouseout: {
-        '.cell[data-row-index]': (event, ui) => {
-          // timer.current && clearTimeout(timer.current);
-          // console.log('%cmouseout', 'background:blue');
-          styleRef.current.innerHTML = '';
-
-          // timer.current = setTimeout(() => {
-          //   // const _ = currHover.current
-          //   // if (_.rowIndex !== null || _.columnIndex !== null ) {
-          //     handleRef.current({ rowIndex: null, columnIndex: null });
-          //   // }
-          // }, 100);
-
-          // window._cnt2 = (window._cnt2 || 0);
-          // console.log('out', window._cnt2++)
-        },
-      },
-    };
-  }, []);
-
-  const eventHandlers = useEventHandlers({ ...props.events, ...ownEvents }, rows);
-
-  console.log('>>>>>>');
-  // console.log(eventsHash);
-  console.log('<<<<<<');
-
-  const renderHeader =
-    props.renderHeader ||
-    ((data) => {
-      return data;
-    });
-
-  const renderCell = (rowIndex, columnIndex) => {
-    const row = rows[rowIndex];
-    const column = columns[columnIndex];
-    const data = row.arr[columnIndex];
-
-    if (row._isHeader) {
-      if (column.header) {
-        return column.header(data);
-      }
-      // return data;
-      return renderHeader(data);
-    }
-    return column.render(data, { rowIndex, columnIndex });
-    // if (rowIndex)
-  };
+  const months = differenceInMonths(maxDate, minDate) + 1;
 
   const Cell = ({ rowIndex, columnIndex, className, style }) => (
     <div className={className} style={style} data-row-index={rowIndex} data-column-index={columnIndex}>
-      {renderCell(rowIndex, columnIndex)}
+      
+
+      {format(
+        addMonths(minDate, rowIndex),
+
+        'YYYY-MM'
+      )}
+      {/* {rowIndex} */}
     </div>
   );
 
-  const fixedTopCount = (props.fixedTopCount || 0) + 1;
+  const rowHeight = () => {
+    return 100;
+  }
 
+  // console.log(months)
   return (
-    <div>
-      <div ref={styleRef} />
-      <pre>{JSON.stringify(hover)}</pre>
-      <div {...eventHandlers}>
-        <WindowGrid
-          {...props}
-          rowCount={rows.length}
-          fixedTopCount={fixedTopCount}
-          columnCount={columns.length}
-          columnWidth={columnWidth}
-        >
-          {Cell}
-        </WindowGrid>
-      </div>
-    </div>
-  );
+    <>
+    <h3>{months}</h3>
+    <pre>{JSON.stringify({minDate, maxDate})}</pre>
+    <WindowGrid
+      width={400}
+      height={500}
+    
+      rowCount={months}
+      rowHeight={rowHeight}
+      columnCount={1}
+      columnWidth={100}
+      fillerColumn='stretch'
+      fitToGrid
+    >
+      {Cell}
+
+    </WindowGrid>
+    </>
+  //   <WindowGrid
+  //   rowCount={100}
+  //   rowHeight=
+  //   {...props}
+  //   rowCount={rows.length}
+  //   fixedTopCount={fixedTopCount}
+  //   columnCount={columns.length}
+  //   columnWidth={columnWidth}
+  // >
+  //   {Cell}
+  // </WindowGrid>
+  )
+
 };
 
 export default WindowCalendar;
